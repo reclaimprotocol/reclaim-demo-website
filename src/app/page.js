e99f4b29fa390bc6feabe665ada7742193f4b768
koushith <koushith97@gmail.com>
2024-05-10 16:04:29 +0530

    updates the demo site

--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,7 @@ export default function Home() {
   const [showQR, setShowQR] = useState(false)
   const [isCopied, setIsCopied] = useState(false)
   const [showConfetti, setShowConfetti] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const [showButton, setShowButton] = useState(true)
 
@@ -55,22 +56,25 @@ export default function Home() {
       await navigator.clipboard.writeText(url);
       setIsCopied(true);
       console.log('Link copied to clipboard');
-    } catch (err) {
+    } catch(err) {
       console.error('Failed to copy link: ', err);
     }
   };
 
   const getVerificationReq = async (providerId) => {
     try {
+      setIsLoaded(true)
       await reclaimClient.buildProofRequest(providerId)
       reclaimClient.setSignature(await reclaimClient.generateSignature(APP_SECRET))
 
       const { requestUrl, statusUrl } = await reclaimClient.createVerificationRequest()
       console.log('requestUrl', requestUrl)
       console.log('statusUrl', statusUrl)
+
       setUrl(requestUrl)
       setShowQR(true)
       setShowButton(false)
+      setIsLoaded(false)
 
       await reclaimClient.startSession({
         onSuccessCallback: proofs => {
@@ -85,7 +89,7 @@ export default function Home() {
           console.log('error', error)
         }
       })
-    } catch (error) {
+    } catch(error) {
       console.error('Error in getVerificationReq', error)
       // Handle error gracefully, e.g., show a notification to the user
       // and possibly revert UI changes made before the error occurred
@@ -105,7 +109,7 @@ export default function Home() {
 
     let isMobileDevice = regexp.test(details);
 
-    if (isMobileDevice) {
+    if(isMobileDevice) {
       setIsMobileDevice(true)
     } else {
       setIsMobileDevice(false)
@@ -115,7 +119,7 @@ export default function Home() {
 
 
   useEffect(() => {
-    if (proofs) {
+    if(proofs) {
       setShowConfetti(true);
       setTimeout(() => {
         setShowConfetti(false);
@@ -150,6 +154,17 @@ export default function Home() {
         {/* {showButton && (<button className="bg-blue-500 mt-8 hover:bg-blue-700 lg:text-lg md:text-base sm:text-lg text-gray-200 font-semibold py-2 px-4 rounded"
           onClick={handleButtonClick}
         >Generate Proof Of Ownership Of  </button>)} */}
+
+        {isLoaded && (<>
+          <div role="status" className='mt-10'>
+            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+            </svg>
+
+
+          </div>
+        </>)}
         {showQR && (
           <>
             {!isMobileDevice && (
@@ -180,7 +195,7 @@ export default function Home() {
             <>
               <h3 className="text-slate-300 text-sm lg:text-2xl md:text-xl sm:text-lg xs:text-xs mt-8">Proofs Received</h3>
               <div style={{ maxWidth: '1000px' }}>
-               <p> {JSON.stringify(proofs?.claimData)}</p>
+                <p> {JSON.stringify(proofs?.claimData)}</p>
 
               </div>
 
