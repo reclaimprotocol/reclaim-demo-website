106e72602d0cacc2e277e7469286fff0c9724ec3
koushith <koushith97@gmail.com>
2024-05-23 02:24:38 +0530

    added a easter egg

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,9 +9,49 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  console.log(`
+  _    _                              _     _      _             _ 
+ | |  | |                            | |   (_)    (_)           | |
+ | |  | | ___      __ _ _ __ ___     | |__  _ _ __ _ _ __   __ _| |
+ | |/\\| |/ _ \\    / _\` | '__/ _ \\    | '_ \\| | '__| | '_ \\ / _\` | |
+ \\  /\\  /  __/   | (_| | | |  __/    | | | | | |  | | | | | (_| |_|
+  \\/  \\/ \\___|    \\__,_|_|  \\___|    |_| |_|_|_|  |_|_| |_|\\__, (_)
+                                                            __/ |  
+                                                            |___/   
+ 
+ If you are here, we're hiring hackers like you - integration engineering. 
+ You will be looking at chrome devtools and building reclaim providers
+ 
+ Apply :
+ https://x.com/madhavanmalolan/status/1792949714813419792
+   `);
   return (
     <html lang="en" className="bg-black">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <!--
+              _    _                                     _   _      _             _
+             | |  | |                                   | |   (_)    (_)           | |
+             | |  | | ___          __ _ _ __ ___        | |__  _ _ __ _ _ __   __ _| |
+              | |/\\| |/ _ \\    / _\` | '__/ _ \\      | '_ \\| | '__| | '_ \\ / _\` | |
+             \\  /\\  /  __/      | (_| | | |  __/      | | | | | |  | | | | | (_| |_|
+              \\/  \\/ \\___|     \\__,_|_|  \\___|     |_| |_|_|_|  |_|_| |_|\\__, (_)
+                                                                               __/ |
+                                                                              |___/
+
+              If you are here, we're hiring hackers like you - integration engineering.
+              You will be looking at chrome devtools and building reclaim providers.
+
+              Apply:
+              https://x.com/madhavanmalolan/status/1792949714813419792
+              -->
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
