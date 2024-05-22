17297277aa8d741785c4db7588402901b7158ad0
koushith <koushith97@gmail.com>
2024-05-23 03:01:12 +0530

    adds easter eggs

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,14 +10,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   console.log(`
-  _    _                              _     _      _             _ 
- | |  | |                            | |   (_)    (_)           | |
- | |  | | ___      __ _ _ __ ___     | |__  _ _ __ _ _ __   __ _| |
- | |/\\| |/ _ \\    / _\` | '__/ _ \\    | '_ \\| | '__| | '_ \\ / _\` | |
+  _    _                                _     _      _             _ 
+ | |  | |                              | |   (_)    (_)           | |
+ | |  | | ___        __ _ _ __ ___    | |__  _ _ __ _ _ __   __ _| |
+ | |/\\| |/ _ \\    / _\` | '__/ _ \\  | '_\\| | '__| | '_ \\ / _\` | |
  \\  /\\  /  __/   | (_| | | |  __/    | | | | | |  | | | | | (_| |_|
-  \\/  \\/ \\___|    \\__,_|_|  \\___|    |_| |_|_|_|  |_|_| |_|\\__, (_)
-                                                            __/ |  
-                                                            |___/   
+  \\/  \\/ \\___|   \\__,_|_|  \\___|  |_| |_|_|_|  |_|_| |_|\\__, (_)
+                                                                __/ |  
+                                                              |___/   
  
  If you are here, we're hiring hackers like you - integration engineering. 
  You will be looking at chrome devtools and building reclaim providers
--- src/app/page.js
+++ src/app/page.js
@@ -32,6 +32,23 @@ const providers = [
 
 
 export default function Home() {
+  console.log(`
+  _    _                              _     _      _             _ 
+ | |  | |                            | |   (_)    (_)           | |
+ | |  | | ___      __ _ _ __ ___     | |__  _ _ __ _ _ __   __ _| |
+ | |/\\| |/ _ \\    / _\` | '__/ _ \\    | '_ \\| | '__| | '_ \\ / _\` | |
+ \\  /\\  /  __/   | (_| | | |  __/    | | | | | |  | | | | | (_| |_|
+  \\/  \\/ \\___|    \\__,_|_|  \\___|    |_| |_|_|_|  |_|_| |_|\\__, (_)
+                                                            __/ |  
+                                                           |___/   
+ 
+If you are here, we're hiring hackers like you - integration engineering. 
+You will be looking at chrome devtools and building reclaim providers
+ 
+Apply :
+https://x.com/madhavanmalolan/status/1792949714813419792
+`);
+
 
   const [url, setUrl] = useState('')
   const [isMobileDevice, setIsMobileDevice] = useState(false)
